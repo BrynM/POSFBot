c68e712f473dc6feba310c66cb084776e10bc4f9
BrynM <sites.posfbot@brynmosher.com>
2014-02-07 04:33:05 +0000

    base comment mention searching works

--- posf/POSFBot.py
+++ posf/POSFBot.py
@@ -1,7 +1,30 @@
-import praw
-import configparser
+import configparser, praw, re, time
 import posf
 import POSFRedis
+
+def read_ini(ini='cfg_posfbot.ini'):
+	global posf
+	opts = {}
+	ints = {
+		'debug_level'   : 1,
+		'get_n_comments': 1,
+		'redis_db'      : 1,
+		'redis_port'    : 1,
+	}
+	bools = {
+		'debug'         : True,
+		'mail_enabled'  : True,
+	}
+	conf = configparser.ConfigParser()
+	conf.read_file(open(ini))
+	for key, value in conf.items('posfbot'):
+		if key in ints:
+			opts[key] = int(value)
+		elif key in bools:
+			opts[key] = str(value) and True
+		else:
+			opts[key] = str(value)
+	return opts
 
 class POSFBot(posf.POSFBase):
 	def __init__(self, options={}):
@@ -10,8 +33,6 @@ class POSFBot(posf.POSFBase):
 		self.body            = ''
 		self.comments        = False
 		self.comment_matches = False
-		self.__last_comment  = False
-		self.__last_epoch    = False
 		self.opts            = {}
 		self.opts_default    = {
 			'user'          : 'reddit_user',
@@ -21,15 +42,18 @@ class POSFBot(posf.POSFBase):
 			'debug_level'   : 1,
 			'get_n_comments': 100,
 			'mail_enabled'  : False,
+			'mention_blurb' : 'Search for mentions',
+			'mention_regex' : 'looking\s*with\s*regular\s*extressions',
+			'mention_subs'  : 'pics+wtf',
 			'recipients'    : 'other_reddit_user',
 			'redis_host'    : '127.0.0.1',
 			'redis_port'    : 6379,
 			'redis_db'      : 0,
 			'redis_auth'    : '',
-			'search_name'   : 'Ima chargin mah search code',
-			'search_rgx'    : 'looking\s*with\s*regular\s*extressions',
-			'subreddits'    : 'pics+wtf'
 		}
+		self.__comments      = False
+		self.__last_comment  = False
+		self.__last_epoch    = False
 		self.__praw          = False
 
 		self.opts         = self.parse_opts(options)
@@ -37,6 +61,9 @@ class POSFBot(posf.POSFBase):
 		self.__cache      = POSFRedis.POSFRedis(self.opts)
 
 		self.debug('Started ' + self.__appstring)
+
+	def __mention_match(self, body):
+		return re.search(self.opts['mention_regex'], body, flags=re.IGNORECASE) and True
 
 	def __p(self):
 		if not self.__praw:
@@ -48,8 +75,47 @@ class POSFBot(posf.POSFBase):
 				raise NameError('* PRAW CONNECTION FAILED *')
 		return self.__praw
 
-	def is_match(self, body):
-		return re.search(self.opts['search_rgx'], body, flags=re.IGNORECASE) and True
+	def get_mentions(self, comments={}):
+		if not comments:
+			if not self.__comments:
+				self.get_comments()
+				if not self.__comments:
+					raise NameError('Could not get mention comments')
+
+		for comment in self.__comments:
+			summary    = ''
+			bod        = comment.body.encode('ascii', 'replace')
+			mention    = self.__mention_match(bod)
+			if mention:
+				found += 1
+				comment_data = {
+					'Subreddit': comment.subreddit.title.encode('ascii', 'replace') + ' (/r/' + comment.subreddit.display_name.encode('ascii', 'replace') + ')',
+					'Post'     : comment.link_title.encode('ascii', 'replace'),
+					'Author'   : comment.author.name.encode('ascii', 'replace') + ' score ' + str(comment.ups - comment.downs) + '(' + str(comment.ups) + '/' + str(comment.downs) + ')',
+					'Time'     : time.ctime(comment.created_utc) + ' ' + self.pretty_date(int(comment.created_utc)),
+					'Name'     : comment.name,
+					'ID'       : comment.id,
+					'Permalink': re.sub('[a-z]{2,}://[^/]+', '', comment.permalink)
+				}
+	
+				summary += '\n########## Found!\n'
+				message += '\n----\n'
+	
+				for key, value in comment_data.iteritems():
+					message += '* **' + key + ':** ' + value + '\n'
+					summary += '# ' + key + ': ' + value + '\n'
+	
+				message += '\n> ' + re.sub('\n\n', '\n\n> ', bod) + '\n\n'
+				summary += bod + '\n\n'
+	
+			else:
+				summary += '# Skipped ' + comment.name + ' ' + time.ctime(comment.created_utc) + ' ' + self.pretty_date(int(comment.created_utc))
+	
+			print summary
+	
+			if comment.created_utc < self.__last_epoch:
+				self.__last_epoch   = comment.created_utc
+				self.__last_comment = comment.name
 
 	def get_comments(self):
 		p                   = self.__p()
@@ -59,53 +125,6 @@ class POSFBot(posf.POSFBase):
 			'count': self.opts['get_n_comments'],
 			'after': self.__last_comment
 		}
-		self.__comments = p.get_comments(self.opts['subreddits'], False, get_comment_args)
+		self.__comments = p.get_comments(self.opts['mention_subs'], False, get_comment_args)
 		return self.__comments
 
-#def get_comment_listing():
-#	global posf, found, last_comment, last_epoch, message, search_rgx
-#
-#	print '########## Getting comments...'
-#
-#	all_comments = posf.get_comments()
-#
-#	print '########## Listing comments...'
-#
-#	for comment in all_comments:
-#		summary    = ''
-#		bod        = comment.body.encode('ascii', 'replace')
-#		mention    = posf.is_match(bod)
-#
-#
-#		if mention:
-#			found += 1
-#			comment_data = {
-#				'Subreddit': comment.subreddit.title.encode('ascii', 'replace') + ' (/r/' + comment.subreddit.display_name.encode('ascii', 'replace') + ')',
-#				'Post'     : comment.link_title.encode('ascii', 'replace'),
-#				'Author'   : comment.author.name.encode('ascii', 'replace') + ' score ' + str(comment.ups - comment.downs) + '(' + str(comment.ups) + '/' + str(comment.downs) + ')',
-#				'Time'     : time.ctime(comment.created_utc) + ' ' + posf.pretty_date(int(comment.created_utc)),
-#				'Name'     : comment.name,
-#				'ID'       : comment.id,
-#				'Permalink': re.sub('[a-z]{2,}://[^/]+', '', comment.permalink)
-#			}
-#
-#			summary += '\n########## Found!\n'
-#			message += '\n----\n'
-#
-#			for key, value in comment_data.iteritems():
-#				message += '* **' + key + ':** ' + value + '\n'
-#				summary += '# ' + key + ': ' + value + '\n'
-#
-#			message += '\n> ' + re.sub('\n\n', '\n\n> ', bod) + '\n\n'
-#			summary += bod + '\n\n'
-#
-#		else:
-#			summary += '# Skipped ' + comment.name + ' ' + time.ctime(comment.created_utc) + ' ' + posf.pretty_date(int(comment.created_utc))
-#
-#		print summary
-#
-#		if comment.created_utc < last_epoch:
-#			last_epoch = comment.created_utc
-#			last_comment = comment.name
-#
-#
--- posfbot.py
+++ posfbot.py
@@ -1,65 +1,9 @@
 #!/usr/bin/python
 
 import configparser
-from posf import posf
+from posf import *
 from posf import POSFRedis
 from posf import POSFBot
 
-# http://docs.python.org/2/library/configparser.html
-
-def read_ini(ini='cfg_posfbot.ini'):
-	global posf
-	opts = {}
-	ints = {
-		'debug_level'   : 1,
-		'get_n_comments': 1,
-		'redis_db'      : 1,
-		'redis_port'    : 1,
-	}
-	bools = {
-		'debug'         : True,
-		'mail_enabled'  : True,
-	}
-	conf = configparser.ConfigParser()
-	conf.read_file(open(ini))
-	for key, value in conf.items('posfbot'):
-		if key in ints:
-			opts[key] = int(value)
-		elif key in bools:
-			opts[key] = str(value) and True
-		else:
-			opts[key] = str(value)
-	return opts
-
-posf  = POSFBot.POSFBot(read_ini())
-posf.get_comments()
-
-
-#print '########## Obtaining PRAW...'
-#
-#r = praw.Reddit('/u/posfbot - /r/picsofsanfrancisco Bot 0.01 by /u/badmonkey0001')
-#
-#print '########## Logging in...'
-#
-#r.login(bot_user, bot_pass)
-#
-#get_comment_listing()
-#
-#print '########## ', found, 'found.'
-#
-#if found > 0 and message != '':
-#	message = 'Found ' + str(found) + ' mentions of ' + search_name + '\n' + message
-#
-#	print '########## Sending message...'
-#	print message
-#
-#	if mail_enabled:
-#		r.send_message(recipients, ('Found mentions of ' + search_name), message)
-#
-#print '########## Done. Last comment:', last_comment, 'at', last_epoch, time.ctime(last_epoch)
-
-# picsofsanfrancisco
-# posfbot/p0s5dundundundundada
-
-# http://www.reddit.com/r/sanfrancisco+bayarea/comments?count=100&after=t3_1wpbd5
-# http://www.reddit.com/r/sanfrancisco+bayarea/comments/.json?count=100&after=t1_cf5o6ge
+posf  = POSFBot.POSFBot(POSFBot.read_ini())
+posf.get_mentions()
